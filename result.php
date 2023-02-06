f2ff075d66d624ce81e86eb8775a16efe330b652
Alberto-Bruno <brunoalberto97@outlook.it>
2023-02-06 18:30:28 +0100

    styles

--- result.php
+++ result.php
@@ -19,17 +19,20 @@ $filtredtext_length = strlen($censured_text);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Bad Words</title>
 </head>
 <body>
-    <div>
-        <div>
-            <h1>Paragrafo originale:</h1>
+    <div class='container text-center d-flex my-5'>
+        <div class='border w-50 mx-3 p-2'>
+            <h2>Paragrafo originale:</h2>
             <p><?php echo $text ?></p>
             <h4>Lunghezza testo: <span><?= $original_length?> </span> </h4>
         </div>
-        <div>
-            <h1>Paragrafo censurato:</h1>
+        <div class='border w-50 mx-3 p-2'>
+            <h2>Paragrafo censurato:</h2>
             <p><?php echo $censured_text ?></p>
             <h4>Lunghezza testo: <span><?= $filtredtext_length?> </span> </h4>
         </div>
